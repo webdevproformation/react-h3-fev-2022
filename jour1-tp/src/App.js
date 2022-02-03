e827d97908aac6a68b90a37e83a96d048a6a430b
Malik H <malik.h@webdevpro.net>
2022-02-03 12:30:36 +0100

    correction

--- jour1-tp/src/App.js
+++ jour1-tp/src/App.js
@@ -4,10 +4,12 @@ import { Deuxieme } from "./composants/Deuxieme" ;
 import Troisieme , {AutreComposant} from './composants/Troisieme';
 import { Quatrieme } from './composants/Quatrieme';
 import { Boucle } from './composants/Boucle';
+import { Exo1 } from './composants/Exo1';
 
 
 function App() {
   return  <>
+    <Exo1 />
     <Premier /> 
     <Deuxieme />
     <Troisieme />
---
+++ jour1-tp/src/composants/Exo1.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export function Exo1(){
+    const data = [
+        { id : 1 , titre : "Article1" , dt_creation : new Date() },
+        { id : 2 , titre : "Article2" , dt_creation : new Date() },
+    ];
+
+    function getDate(dt){
+        return dt.getDate() + "/" + (dt.getMonth() + 1) + "/" + dt.getFullYear()
+    }
+    // janvier => getMonth() => 0
+    // decembre => getMonth() => 11
+
+    // 03/02/2022
+
+    return <React.Fragment>
+    {data.map((article , index) => {
+        const {titre , dt_creation } = article ; // déstructuration article qui est { id : 1 , titre : "Article1" , dt_creation : new Date() }
+        return <article key={index}> 
+        <h2>{ titre }</h2>
+        <time>{ getDate(dt_creation) }</time>
+    </article>
+    }
+    )}
+    </React.Fragment>
+}
