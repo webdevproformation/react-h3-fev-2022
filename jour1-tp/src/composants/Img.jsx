6e58da3dbde16a2a8dfa09442d15c94f2d3af4b0
Malik H <malik.h@webdevpro.net>
2022-02-03 16:46:11 +0100

    ajouter des images des images dans vos vues

--- jour1-tp/src/composants/Img.jsx
+++ jour1-tp/src/composants/Img.jsx
@@ -1,9 +1,10 @@
 import toto from "./fleur.jpg";
-import { Fragment } from "react"
+import { Fragment } from "react";
+import "./Img.css" ; 
 
 export const Img = () => {
     return <Fragment>
-        <img src="img/mario.jpg" alt="" width="400px" />
-        <img src={toto} alt="" width="400px" />
+            <img src="img/mario.jpg" alt="" className="img1" />
+            <img src={toto} alt="" className="img2" />
         </Fragment>
 } 
