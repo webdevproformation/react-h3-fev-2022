415b5a73ee6fc482b8ee71b9c5cd241c8cb6167f
Malik H <malik.h@webdevpro.net>
2022-02-03 11:25:39 +0100

    composant fonctionnel

--- jour1-tp/src/App.js
+++ jour1-tp/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Premier from "./composants/Premier" ;
 import { Deuxieme } from "./composants/Deuxieme" ;
+import Troisieme , {AutreComposant} from './composants/Troisieme';
 
 function App() {
   return  <>
     <Premier /> 
     <Deuxieme />
+    <Troisieme />
+    <AutreComposant />
   </>
 }
 
---
+++ jour1-tp/src/composants/Troisieme.jsx
@@ -0,0 +1,22 @@
+//import React from "react";
+
+const Troisieme = () => {
+    return (
+        <>
+            <h2>troisième</h2>
+            <ul>
+                <li>element1</li>
+                <li>element2</li>
+            </ul>
+        </>
+    )
+}
+export default Troisieme ; // import Troisieme from "./.../Troisieme"
+
+export const AutreComposant = () => { // import {AutreComposant } from "./.../Troisieme"
+    return <p>autre</p>
+}
+
+// export Troisieme ; // import { Troisieme } from "./.../Troisieme"
+
+// function Troisieme(){}
