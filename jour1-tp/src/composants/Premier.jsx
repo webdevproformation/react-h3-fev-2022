94f164dc803b662fb58acbb64e172d68bcc731ae
Malik H <malik.h@webdevpro.net>
2022-02-03 11:06:29 +0100

    premier composant class

---
+++ jour1-tp/src/composants/Premier.jsx
@@ -0,0 +1,14 @@
+// minimum pour créer
+
+import { Component } from "react"
+
+class Premier extends Component {
+    render(){
+        return <p>Voici notre premier composant sous forme de class</p>
+    }
+}
+
+export default Premier ; 
+
+// un fois que le composant est créé => importer dans un composant composant racine App.js
+
