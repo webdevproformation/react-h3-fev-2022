e9c0ce3abf6fa2b8b8c80d007269faafc2ac620c
Malik H <malik.h@webdevpro.net>
2022-02-03 11:17:02 +0100

    correction

--- jour1-tp/src/App.js
+++ jour1-tp/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import Premier from "./composants/Premier" ;
+import { Deuxieme } from "./composants/Deuxieme" ;
 
 function App() {
-  return  <Premier />
+  return  <>
+    <Premier /> 
+    <Deuxieme />
+  </>
 }
 
 export default App;
---
+++ jour1-tp/src/composants/Deuxieme.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export class Deuxieme extends React.Component{
+    render(){
+        return <>
+             <h1>je suis le deuxième composant</h1>
+             <p>lorem ipsum</p>
+        </>
+    }
+}
