b838a8cae0dfeb62886c644c9d070a8cf8d02cc4
Malik H <malik.h@webdevpro.net>
2022-02-04 10:52:55 +0100

    réaliser un update

--- jour2-tp/src/composants/Articles.jsx
+++ jour2-tp/src/composants/Articles.jsx
@@ -1,4 +1,4 @@
-import { useState , useEffect } from "react";
+import { useState } from "react";
 import { Article } from "./Article";
 
 export function Articles(){
@@ -8,18 +8,33 @@ export function Articles(){
         {id : 3 , titre : "Article 3" , contenu : "lorem isum" , dt_creation : Date.now() , like : 10}
     ] )
 
-    useEffect(function(){
-        setData(data);
-    }, [data] )
+    function augmenterV1 ( idArticle ){ // il est possible d'envoyer des fonctions dans les props 
+        // mutabilité des données 
+        // 1 "toto" false
+        // []
+        // data => l'impression de le modifier mais en fait non
+        const cloneData = [...data]; // copie 
+        const articleAmodifier = data.find( d => d.id === idArticle)
+        const index = data.indexOf(articleAmodifier);
+        // data[index].like++ // impression de modifier la variable data => pas de modification  
+        //erreur 
+        cloneData[index].like++ // modifier la valeur d'une copie de data 
+        cloneData[index].titre = "titre modifié" ; 
+        setData(cloneData);
+        // console.log(data); 
+    }
 
-    function augmenter ( idArticle ){ // il est possible d'envoyer des fonctions dans les props 
-        const articleAModifier = data.find( d => d.id === idArticle );
-        const index = data.indexOf(articleAModifier);
-        data[index].like++ ; // modification effectuée
-        console.log(data);
-        setData(data);
-        console.log(data);
-        // traitement setData()
+    function augmenter(idArticle){
+        // const cloneData = data.map() // parcourir le tableau copier la modification dans un nouveau tableau
+        const cloneData = data.map(function(d){
+            if(d.id === idArticle){
+                //return {...d , like : d.like + 1}
+                return Object.assign({}, d , {like : d.like + 1})
+            }
+            return d ;
+        })
+
+        setData( cloneData );
     }
 
     return <div className="row">
