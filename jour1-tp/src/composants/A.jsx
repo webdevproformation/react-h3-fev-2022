0826096f64cd658ccd3fdbf4d1958327ababb6ce
Malik H <malik.h@webdevpro.net>
2022-02-04 09:20:39 +0100

    start jour2

--- jour1-tp/src/composants/A.jsx
+++ jour1-tp/src/composants/A.jsx
@@ -1,7 +1,17 @@
-export const A = ( {...props } ) => { // props = { source:"http", couleur:"rouge", show:"false" }
+export const A = ( props ) => { // props = { source:"http", couleur:"rouge", show:"false" }
     if(! props.show) 
     console.log(props)
     return <p></p>
 }
 
+// donner une valeur par défaut à une props
+// https://www.reactjstutorials.com/react-basics/17/react-default-props
+A.defaultProps = {
+    show : false
+}
+
+// https://www.reactjstutorials.com/react-basics/19/react-prop-types
+
 // rdv 9h00 demain pour la suite !! 
+
+
